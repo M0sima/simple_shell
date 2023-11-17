ed901b194a29603f673f6b28f38ea00a4f21ef88
M0sima <mosimam3@gmail.com>
2023-11-17 11:59:05 -0800

    shell

---
+++ errors.c
@@ -0,0 +1,80 @@
+#include "shell.h"
+
+/**
+ * _eputs - prints an input string
+ * @str: the string
+ * Return: Nothing
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar -  the character c to stderr
+ * @c: The character
+ * Return: On success 1.
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - the character c to given fd
+ * @c: The character
+ * @fd: The filedescriptor to write
+ * Return: On success 1.
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putsfd - prints an input string
+ * @str: the string
+ * @fd: the filedescriptor to write
+ * Return: the num of chars
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
+
